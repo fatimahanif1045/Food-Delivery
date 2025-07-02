77fe337333657fd2d91b7745befda6527190217d
fatimahanif1045 <fatimahanif1045@gmail.com>
2025-07-02 09:46:54 +0500

    resolved integration issues

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -27,13 +27,13 @@ const Footer = () => {
         <div className="footer-content-right">
             <h2>GET IN TOUCH</h2>
             <ul>
-                <li>+1-212-456-7890</li>
-                <li>contact@tomato.com</li>
+                <li>+923208518545</li>
+                <li>fatimahanif1045@gmail.com</li>
             </ul>
         </div>
       </div>
       <hr />
-      <p className="footer-copyright">Copyright 2024 © Tomato.com - All Right Reserved.</p>
+      <p className="footer-copyright">Copyright 2025 © Tomato.com - All Right Reserved.</p>
     </div>
   )
 }
